2d4c3818a6f9013b6420d050c6bf14e64062272e
Abhishek8115 <53142909+Abhishek8115@users.noreply.github.com>
2025-06-06 00:47:06 +0530

    Update JenkinsApplication.java

--- jenkins/src/main/java/com/CICD/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/CICD/jenkins/JenkinsApplication.java
@@ -8,7 +8,7 @@ public class JenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
-		System.out.print("Hello world");
+		System.out.print("Hello world its me");
 	}
 
 }
